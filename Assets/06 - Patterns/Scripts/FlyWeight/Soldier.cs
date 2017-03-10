3b97820c90f547d6e9476dd603926a11a9b8dcf3
Héctor C. Alzate <halzate93@gmail.com>
2017-03-10 00:12:17 -0500

    Singleton

---
+++ Assets/06 - Patterns/Scripts/FlyWeight/Soldier.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Patterns
+{
+	public class Soldier : MonoBehaviour {
+		public int life;
+		public int attack;
+		public int defense;
+
+		[SerializeField]
+		private SoldierMind mind;
+
+		public void OnTurn ()
+		{
+			mind.UpdateSoldier (this);
+		}
+
+		public void Attack ()
+		{
+			Debug.Log ("Attack");	
+		}
+
+		public void Run ()
+		{
+			Debug.Log ("Run");	
+		}
+	}
+}
---
+++ Assets/Scripts/Singleton/CoinPickup.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class CoinPickup : MonoBehaviour 
+{
+	public string coinTag;
+	public int pointsPerCoin;
+
+	private void OnTriggerEnter2D (Collider2D other)
+	{
+		if (other.CompareTag (coinTag))
+		{
+			Score.Instance.AddPoints (pointsPerCoin);
+			Destroy (other.gameObject);
+		}
+	}
+}
